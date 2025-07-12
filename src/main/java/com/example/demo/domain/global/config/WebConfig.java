301e22c02b6200c8d664ad1d90dc24d65d7896d8
dilmah0203 <dilmah0203@naver.com>
2025-07-13 03:19:07 +0900

    refactor:개행 변경

--- src/main/java/com/example/demo/domain/global/config/WebConfig.java
+++ src/main/java/com/example/demo/domain/global/config/WebConfig.java
@@ -5,7 +5,8 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
- * @Configuration : 스프링 빈으로 등록하기 위한 애노테이션 WebMvcConfigurer 인터페이스는 스프링의 기본 mvc 구성을 확장 및 수정할 수 있다.
+ * @Configuration : 스프링 빈으로 등록하기 위한 애노테이션
+ * WebMvcConfigurer 인터페이스는 스프링의 기본 mvc 구성을 확장 및 수정할 수 있는 애노테이션으로
  * 인터셉터 설정, CORS, 메세지 컨버터 설정을 할 수 있다.
  */
 @Configuration
